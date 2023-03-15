ab7a11146fe3e2cbbb522bbcb111a2bad2d5b5cb
EmilyBernard <bernam2000@hotmail.com>
2023-03-15 13:01:56 -0400

    refined HTML, added vars and timestamp to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-var APIKey = "ad431daf2b902fd2d8a3b4c76d3a4c0f"
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+//Capture cuurent date,time
+function update() {
+    $("#time").html(dayjs().format("MMMM D YYYY, H:mm:ss"));
+  }
+  setInterval(update, 1000);
+  var currentTime = dayjs().format("HH");
+
+//declare variables  
+  var APIKey = "ad431daf2b902fd2d8a3b4c76d3a4c0f"
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+  var button = document.getElementById("button");
+  var data = document.getElementsByClassName("wicon");
+  var searchVal = document.querySelector(".searchVal");
+  var city;
+ //var city = $(".searchVal").val().trim();
+  var locationIcon = document.querySelector("#icon");
+  
 
